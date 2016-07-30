26692a2449e87ff0797f7c55555be06502599820
laoshu133 <admin@laoshu133.com>
2016-07-30 12:02:02 +0800

    Site updated: 2016-07-30 12:02:02

--- Lab/webqq/script/global.js
+++
@@ -1,8 +0,0 @@
-﻿/********页面全局函数********/
-
-//获取当前屏幕的 W,H
-var ss=function(){return{width:self.innerWidth || (document.documentElement.clientWidth || document.body.clientWidth),
-height:self.innerHeight || (document.documentElement.clientHeight || document.body.clientHeight)}}
-
-//是否为IE浏览器
-var isIE=(navigator.appName.indexOf('Microsoft')!=-1)?true:false;
